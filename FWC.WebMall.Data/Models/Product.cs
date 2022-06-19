07f888f5412f28e8ac97ae01c5b1949b5e9cb435
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2022-06-19 13:32:35 +0500

    generic repository added

---
+++ FWC.WebMall.Data/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWC.WebMall.Data.Models
+{
+    internal class Product
+    {
+    }
+}
---
+++ FWC.WebMall.Data/ORMHelpers/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FWC.WebMall.Data.ORMHelpers
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        Product GetProductById(int productId);
+       void DeleteProduct(int productId);
+   IEnumerable<Product> GetProducts();
+      void InsertProduct(Product product);
+       void Save();
+        void UpdateProduct(Product product);
+    }
+}
